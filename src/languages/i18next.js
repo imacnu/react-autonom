e75badeb8e3b7a9b3b3543d31b6f01d27b56633a
Manu Álvarez <manuelangel.alvarez.next@bbva.com>
2020-05-16 13:32:22 +0200

    add internalization

---
+++ src/languages/i18next.js
@@ -0,0 +1,14 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json";
+import es from "./es.json";
+
+i18n.use(initReactI18next).init({
+  lng: "en",
+  resources: {
+    en,
+    es,
+  },
+});
+
+export default i18n;
